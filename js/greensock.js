91547e75365a94d41d5b054588589e3cb48fdfb8
Ratatouille <cshim201@gmail.com>
2022-10-23 17:23:03 -0400

    supdate

---
+++ js/greensock.js
@@ -0,0 +1,90 @@
+gsap.registerPlugin(ScrollTrigger);
+
+function intro() {
+    var tl = gsap.timeline();
+    tl.from(".gsap1", {
+        y:-100,
+        stagger:0.15,
+        opacity:0,
+        duration: 0.5,
+    })
+
+    tl.from(".gsap2", {
+        y:-50,
+        stagger:0.15,
+        opacity:0,
+        duration: 0.5,
+    })
+    
+    tl.from(".gsap3", {
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap4", {
+        scrollTrigger:{
+            trigger:".gsap4",
+            start:"top 50%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap5", {
+        scrollTrigger:{
+            trigger:".gsap5",
+            start:"top 60%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap6", {
+        scrollTrigger:{
+            trigger:".gsap6",
+            start:"top 60%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap7", {
+        scrollTrigger:{
+            trigger:".gsap7",
+            start:"top 60%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap8", {
+        scrollTrigger:{
+            trigger:".gsap8",
+            start:"top 60%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap9", {
+        scrollTrigger:{
+            trigger:".gsap9",
+            start:"top 60%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+    gsap.from(".gsap10", {
+        scrollTrigger:{
+            trigger:".gsap9",
+            start:"top 60%"
+        },
+        opacity:0,
+        duration: 1,
+    })
+
+}
+
+
+intro();
