3f26f5552ba8db38d9c92b2515e987157c89dd55
Ratatouille <cshim201@gmail.com>
2023-01-11 11:43:34 -0500

    .

--- greensock.js
+++ greensock.js
@@ -1,5 +1,176 @@
 gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".gsap6", {
+   scrollTrigger:{
+      trigger:".about-trigger",
+      start: "top 30%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap7", {
+   scrollTrigger:{
+      trigger:".about-trigger",
+      start: "top 30%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap8", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 60%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap9", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 60%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap10", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 60%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap11", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 40%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap12", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 40%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap13", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 20%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap14", {
+   scrollTrigger:{
+      trigger:".project-trigger",
+      start: "top 20%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap15", {
+   scrollTrigger:{
+      trigger:".gsap14",
+      start: "top 50%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap16", {
+   scrollTrigger:{
+      trigger:".preloader-trigger",
+      start: "top 60%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap17", {
+   scrollTrigger:{
+      trigger:".preloader-trigger",
+      start: "top 60%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap18", {
+   scrollTrigger:{
+      trigger:".preloader-trigger",
+      start: "top 40%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap19", {
+   scrollTrigger:{
+      trigger:".preloader-trigger",
+      start: "top 40%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap20", {
+   scrollTrigger:{
+      trigger:".preloader-trigger",
+      start: "top 20%"
+   },
+ x: -100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap21", {
+   scrollTrigger:{
+      trigger:".preloader-trigger",
+      start: "top 20%"
+   },
+ x: 100,
+ duration: 1,
+ opacity:0,
+});
+
+gsap.from(".gsap22", {
+   scrollTrigger:{
+      trigger:".end-trigger",
+      start: "top 60%"
+   },
+ y: 100,
+ duration: 1,
+ opacity:0,
+});
+
+
 function intro() {
    var navbar = gsap.timeline();
    navbar.from(".gsap1", {
@@ -33,6 +204,10 @@ function intro() {
       y:25,
    })
 
+
+
 }
+
+
 
 intro()
